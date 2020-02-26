772079701aff281e04d33e1e5aae988db1f58d3c
David Grove <groved@us.ibm.com>
2020-02-26 12:53:15 -0500

    also push an image to kar-stage on git tag operations

--- travis/pushImages.sh
+++ travis/pushImages.sh
@@ -12,8 +12,11 @@ docker login -u iamapikey -p "${DOCKER_APIKEY}" us.icr.io
 
 DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
 
-# if doing nightly also push a tag with the hash commit
 if [ ${IMAGE_TAG} == "nightly" ]; then
+  # if doing nightly also push a tag with the hash commit
   SHORT_COMMIT=`git rev-parse --short HEAD`
   DOCKER_IMAGE_TAG=dev-${SHORT_COMMIT} make dockerPush
+else
+  # if running because of a git tag operation, also push to kar-staging
+  DOCKER_NAMESPACE=kar-stage DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
 fi
