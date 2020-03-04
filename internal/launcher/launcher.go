44d012ea2c962bcfd104e9f7817bb85696556033
Olivier Tardieu <tardieu@us.ibm.com>
2020-03-04 10:07:07 -0500

    Properly close stdout and stderr in launcher

--- internal/launcher/launcher.go
+++ internal/launcher/launcher.go
@@ -6,12 +6,16 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"sync"
 
 	"github.ibm.com/solsa/kar.git/pkg/logger"
 )
 
+var wg = sync.WaitGroup{}
+
 // dump adds a time stamp and a prefix to each line of a log
 func dump(prefix string, in io.Reader) {
+	defer wg.Done()
 	scanner := bufio.NewScanner(in)
 	for scanner.Scan() {
 		log.Print(prefix, scanner.Text())
@@ -26,18 +30,21 @@ func Run(args, env []string) {
 	cmd.Stdin = os.Stdin
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		logger.Error("failed to capture stdout from service: %v", err)
+		logger.Fatal("failed to capture stdout from service: %v", err)
 	}
-	go dump("[STDOUT] ", stdout)
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		logger.Error("failed to capture stderr from service: %v", err)
+		logger.Fatal("failed to capture stderr from service: %v", err)
 	}
-	go dump("[STDERR] ", stderr)
-
 	if err := cmd.Start(); err != nil {
-		logger.Error("failed to start service: %v", err)
+		logger.Fatal("failed to start service: %v", err)
 	}
+
+	wg.Add(1)
+	go dump("[STDOUT] ", stdout)
+	wg.Add(1)
+	go dump("[STDERR] ", stderr)
+	wg.Wait()
 
 	if err := cmd.Wait(); err != nil {
 		if v, ok := err.(*exec.ExitError); ok {
