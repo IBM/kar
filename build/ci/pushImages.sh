f2dca9357ebe181127a4979554cb5a20f00cf1e5
David Grove <groved@us.ibm.com>
2020-03-12 15:02:00 -0400

    refine image building/tagging strategy

--- build/ci/pushImages.sh
+++ build/ci/pushImages.sh
@@ -5,18 +5,26 @@ set -eux
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../../"
 
-IMAGE_TAG=$1
+BRANCH=$1
+IMAGE_TAG=$2
 cd $ROOTDIR
 
 docker login -u iamapikey -p "${DOCKER_APIKEY}" us.icr.io
 
-DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
+SHORT_COMMIT=`git rev-parse --short HEAD`
 
-if [ ${IMAGE_TAG} == "latest" ]; then
-  # if doing latest also push a tag with the hash commit
-  SHORT_COMMIT=`git rev-parse --short HEAD`
-  DOCKER_IMAGE_TAG=dev-${SHORT_COMMIT} make dockerPush
+if [ ${BRANCH} == "master" && ${IMAGE_TAG} == "latest" ]; then
+    # push commit hash tagged images
+    DOCKER_NAMESPACE=kar-dev DOCKER_IMAGE_TAG=dev-${SHORT_COMMIT} make dockerPush
+
+    # push `latest` tag images
+    DOCKER_NAMESPACE=kar-dev DOCKER_IMAGE_TAG=latest make dockerPush
 else
-  # if running because of a git tag operation, also push to kar-staging
-  DOCKER_NAMESPACE=kar-stage DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
+    if [ ${BRANCH} == ${IMAGE_TAG} ]; then
+        # A git tag operation, push to kar-prod
+        DOCKER_NAMESPACE=kar-prod DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
+    else
+        # A push to some branch; push commit-taged image to kar-stage
+        DOCKER_NAMESPACE=kar-stage DOCKER_IMAGE_TAG=dev-${SHORT_COMMIT} make dockerPush
+    fi
 fi
