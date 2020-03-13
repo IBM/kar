10bb511a8f628b7b5fc05fb1616b0747ee8e7a08
David Grove <groved@us.ibm.com>
2020-03-13 15:51:33 -0400

    first working shutdown of client & server for looptest

--- examples/incr/server.js
+++ examples/incr/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const { logger, preprocessor, postprocessor, shutdown } = require('./kar')
 
+async function doShutdown (reg, res) {
+  console.log('Shutting down service')
+  await shutdown()
+  await server.close(() => process.exit())
+}
+
 const app = express()
 
 app.use(logger, preprocessor)
@@ -10,11 +16,8 @@ app.post('/incr', (req, res) => {
   res.json(req.body + 1)
 })
 
-app.post('/shutdown', (reg, res) => {
-  console.log('Shutting down service')
-  shutdown()
-})
+app.post('/shutdown', doShutdown)
 
 app.use(postprocessor)
 
-app.listen(process.env.KAR_APP_PORT, '127.0.0.1')
+const server = app.listen(process.env.KAR_APP_PORT, '127.0.0.1')
