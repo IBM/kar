25b7d695380008d1b2e84745e5c13b384e629a73
Olivier Tardieu <tardieu@us.ibm.com>
2020-03-19 09:49:05 -0400

    Fix slow test

--- examples/incr/slow.js
+++ examples/incr/slow.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const { logger, preprocessor, postprocessor } = require('kar')
+const { logger, jsonParser, errorHandler } = require('kar')
 
 const app = express()
 
-app.use(logger, preprocessor)
+app.use(logger, jsonParser)
 
 app.post('/incr', (req, res) => {
   console.log('incr', req.body)
   setTimeout(() => res.json(req.body + 1), 10000)
 })
 
-app.use(postprocessor)
+app.use(errorHandler)
 
 app.listen(process.env.KAR_APP_PORT, '127.0.0.1')
