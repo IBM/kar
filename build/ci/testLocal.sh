3672423606bf9a37ee8b98e2d811f16c4d0de960
David Grove <groved@us.ibm.com>
2020-03-31 16:50:01 -0400

    add local test of actors-ykt

--- build/ci/testLocal.sh
+++ build/ci/testLocal.sh
@@ -8,12 +8,21 @@ ROOTDIR="$SCRIPTDIR/../.."
 . $ROOTDIR/scripts/kar-kind-env.sh
 
 # Run incr/test-harness.js locally
-echo "Executing incr/test-harness.js"
+echo "*** Executing incr/test-harness.js ***"
 
 cd $ROOTDIR/examples/incr
 npm install
 
 kar -app myApp -service myService -actors Foo node server.js &
 sleep 1
-
 kar -app myApp -service myService node test-harness.js
+
+# Run actors-ykt locally
+echo "*** Executing actors-yktr/ykt-client.js ***"
+
+cd $ROOTDIR/examples/actors-ykt
+npm install
+
+./deploy/runServerLocally.sh &
+sleep 1
+./deploy/runClientLocally.sh
