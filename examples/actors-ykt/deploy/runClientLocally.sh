0079b893eeee5b5b01066651161afefa9dc20210
David Grove <groved@us.ibm.com>
2020-04-27 21:33:51 -0400

    avoid running parition0 on ykt-client pod

--- examples/actors-ykt/deploy/runClientLocally.sh
+++ examples/actors-ykt/deploy/runClientLocally.sh
@@ -6,4 +6,4 @@ CODEDIR="$SCRIPTDIR/.."
 KAR_VERBOSE=${KAR_VERBOSE:="info"}
 KAR_RECV_PORT=${KAR_REVC_PORT:=30666}
 
-kar  -v $KAR_VERBOSE -recv $KAR_RECV_PORT -app ykt -service client node $CODEDIR/ykt-client.js
+kar  -v $KAR_VERBOSE -recv $KAR_RECV_PORT -partition_zero_ineligible -app ykt -service client node $CODEDIR/ykt-client.js
