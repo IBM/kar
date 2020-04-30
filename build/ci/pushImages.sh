f1d6fef258d9a32ea7839d3a1994c10d36147bad
David Grove <groved@us.ibm.com>
2020-04-30 11:37:15 -0400

    change travisci setup to push docker images to us.icr.io/research

--- build/ci/pushImages.sh
+++ build/ci/pushImages.sh
@@ -9,22 +9,22 @@ BRANCH=$1
 IMAGE_TAG=$2
 cd $ROOTDIR
 
-docker login -u iamapikey -p "${DOCKER_APIKEY}" us.icr.io
+docker login -u iamapikey -p "${DOCKER_APIKEY_RIS}" us.icr.io
 
 SHORT_COMMIT=`git rev-parse --short HEAD`
 
 if [ ${BRANCH} == "master" ] && [ ${IMAGE_TAG} == "latest" ]; then
     # push commit hash tagged images
-    DOCKER_NAMESPACE=kar-dev DOCKER_IMAGE_TAG=dev-${SHORT_COMMIT} make dockerPush
+    DOCKER_NAMESPACE=research/kar-dev DOCKER_IMAGE_TAG=dev-${SHORT_COMMIT} make dockerPush
 
     # push `latest` tag images
-    DOCKER_NAMESPACE=kar-dev DOCKER_IMAGE_TAG=latest make dockerPush
+    DOCKER_NAMESPACE=research/kar-dev DOCKER_IMAGE_TAG=latest make dockerPush
 else
     if [ ${BRANCH} == ${IMAGE_TAG} ]; then
         # A git tag operation, push to kar-prod
-        DOCKER_NAMESPACE=kar-prod DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
+        DOCKER_NAMESPACE=research/kar-prod DOCKER_IMAGE_TAG=${IMAGE_TAG} make dockerPush
     else
         # A push to some branch; push commit-taged image to kar-stage
-        DOCKER_NAMESPACE=kar-stage DOCKER_IMAGE_TAG=${BRANCH}-${SHORT_COMMIT} make dockerPush
+        DOCKER_NAMESPACE=research/kar-stage DOCKER_IMAGE_TAG=${BRANCH}-${SHORT_COMMIT} make dockerPush
     fi
 fi
