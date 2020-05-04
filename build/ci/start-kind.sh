2767e439ab2b59925a91b9e11013211c9c2ef135
David P Grove <groved@us.ibm.com>
2020-05-04 10:52:27 -0400

    guard rails to better check kind version mismatches (#68)

--- build/ci/start-kind.sh
+++ build/ci/start-kind.sh
@@ -4,15 +4,30 @@
 # controller on it.  This enables services running on the cluster to be
 # exposed by creating Ingress instances.
 
-TRAVIS_KUBE_VERSION=v1.16.9
+# kind version that matches below tags
+KIND_EXPECTED_VERSION=v0.8.1
+
+# Valid node tags for kind 0.8.1
+# Kubernetes 1.18: v1.18.2@sha256:7b27a6d0f2517ff88ba444025beae41491b016bc6af573ba467b70c5e8e0d85f
+# Kubernetes 1.17: v1.17.5@sha256:ab3f9e6ec5ad8840eeb1f76c89bb7948c77bbf76bcebe1a8b59790b8ae9a283a
+# Kubernetes 1.16: v1.16.9@sha256:7175872357bc85847ec4b1aba46ed1d12fa054c83ac7a8a11f5c268957fd5765
+# Kubernetes 1.15: v1.15.11@sha256:6cc31f3533deb138792db2c7d1ffc36f7456a06f1db5556ad3b6927641016f50
+KIND_NODE_TAG=${KIND_NODE_TAG:="v1.16.9@sha256:7175872357bc85847ec4b1aba46ed1d12fa054c83ac7a8a11f5c268957fd5765"}
 
 set -eu
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../.."
 
+# Validate kind version is as expected
+KIND_ACTUAL_VERSION=$(kind version | awk '/ /{print $2}')
+if [ "$KIND_ACTUAL_VERSION" != "$KIND_EXPECTED_VERSION" ]; then
+    echo "Kind version mismatch: expected $KIND_EXPECTED_VERSION but found $KIND_ACTUAL_VERSION"
+    exit 1
+fi
+
 # Boot cluster
-kind create cluster --config ${SCRIPTDIR}/kind-cluster.yaml --name kind --image kindest/node:${TRAVIS_KUBE_VERSION} --wait 10m || exit 1
+kind create cluster --config ${SCRIPTDIR}/kind-cluster.yaml --name kind --image kindest/node:${KIND_NODE_TAG} --wait 10m || exit 1
 
 echo "KIND cluster is running and reachable"
 kubectl get nodes
