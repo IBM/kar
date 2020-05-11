ed85a272bcf53a2f66dedfa77d25138afffa332d
Olivier Tardieu <tardieu@us.ibm.com>
2020-05-11 11:18:59 -0400

    rename method to route in example

--- examples/express/client.js
+++ examples/express/client.js
@@ -1,15 +1,15 @@
 // retry http requests up to 10 times over 10s
 const fetch = require('fetch-retry')(require('node-fetch'), { retries: 10 })
 
-// request url for a given KAR service and method on that service
-function url (service, method) {
-  return `http://127.0.0.1:${process.env.KAR_RUNTIME_PORT}/kar/v1/service/${service}/call/${method}`
+// request url for a given KAR service and route on that service
+function url (service, route) {
+  return `http://127.0.0.1:${process.env.KAR_RUNTIME_PORT}/kar/v1/service/${service}/call/${route}`
 }
 
 // main method
 async function main () {
   // an http request with text/plain content type
-  const res1 = await fetch(url('greeter', 'helloText'), {
+  const res1 = await fetch(url('greeter', '/helloText'), {
     method: 'POST',
     body: 'John Doe',
     headers: { 'Content-Type': 'text/plain' }
@@ -20,7 +20,7 @@ async function main () {
   console.log(text)
 
   // an http request with application/json content type
-  const res2 = await fetch(url('greeter', 'helloJson'), {
+  const res2 = await fetch(url('greeter', '/helloJson'), {
     method: 'POST',
     body: JSON.stringify({ name: 'Jane Doe' }),
     headers: { 'Content-Type': 'application/json' }
