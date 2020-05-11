5738727b3090ad24be622edb06f4ff50284e7498
Olivier Tardieu <tardieu@us.ibm.com>
2020-05-11 09:54:45 -0400

    Add example bypassing JavaScript sdk.

---
+++ examples/express/client.js
@@ -0,0 +1,35 @@
+// retry http requests up to 10 times over 10s
+const fetch = require('fetch-retry')(require('node-fetch'), { retries: 10 })
+
+// request url for a given KAR service and method on that service
+function url (service, method) {
+  return `http://localhost:${process.env.KAR_RUNTIME_PORT}/kar/v1/service/${service}/call/${method}`
+}
+
+// main method
+async function main () {
+  // an http request with text/plain content type
+  const res1 = await fetch(url('greeter', 'helloText'), {
+    method: 'POST',
+    body: 'John Doe',
+    headers: { 'Content-Type': 'text/plain' }
+  })
+
+  // parse response body to text
+  const text = await res1.text()
+  console.log(text)
+
+  // an http request with application/json content type
+  const res2 = await fetch(url('greeter', 'helloJson'), {
+    method: 'POST',
+    body: JSON.stringify({ name: 'Jane Doe' }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  // parse response body to json
+  const obj = await res2.json()
+  console.log(obj.greetings)
+}
+
+// invoke main
+main()
