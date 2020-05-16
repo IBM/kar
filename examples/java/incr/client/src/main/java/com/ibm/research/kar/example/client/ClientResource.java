8dfbac60bd1fc593c610788f30cdb6728159d1c5
David Grove <groved@us.ibm.com>
2020-05-16 08:19:20 -0400

    return JsonValue from Kar.call APIs
    small refactor to push boilerplate code to extract JsonValue from
    Response into the Kar SDK wrapper layer.
    
    Also change Kar.tell APIs to return void instead of Response.

--- examples/java/incr/client/src/main/java/com/ibm/research/kar/example/client/ClientResource.java
+++ examples/java/incr/client/src/main/java/com/ibm/research/kar/example/client/ClientResource.java
@@ -2,6 +2,8 @@ package com.ibm.research.kar.example.client;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.JsonNumber;
+import javax.json.JsonValue;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -9,8 +11,6 @@ import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import java.math.BigDecimal;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -29,63 +29,54 @@ public class ClientResource {
 
 	@Inject
 	private Kar karClient;
-	
+
 	@Inject
 	@RestClient
 	private IncrServer defaultRestClient;
-	
+
 
 	@POST
 	@Path("incrSync")
 	public Response call(KarParams karParams) throws ProcessingException {
 
 		try {
-			
+
 			if (useKar == true) {
-			
-				return this.karClient.call(karParams.service, karParams.path, karParams.params);
-				
+				JsonValue result = karClient.call(karParams.service, karParams.path, karParams.params);
+				Response resp = Response.status(Response.Status.OK).entity(result).build();
+				return resp;
 			} else {
-				
-				BigDecimal bg = (BigDecimal)karParams.params.get("number");
+				JsonNumber bg = (JsonNumber)karParams.params.get("number");
 				int num = bg.intValue();
-				
+
 				Number numObj = new Number();
 				numObj.setNumber(num);
-				
+
 				Number respNum = this.defaultRestClient.incrNumber(numObj);
-				
+
 				Response resp = Response.status(Response.Status.OK).entity(respNum).build();
 				return resp;
-				
 			}
 		} catch (Exception ex) {
-			
+
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
 		}
 
 	}
-	
+
 	@POST
 	@Path("incrAsync")
 	public Response tell(KarParams karParams) throws ProcessingException {
-
 		try {
-			
 			if (useKar == true) {
-			
-				return this.karClient.tell(karParams.service, karParams.path, karParams.params);
-				
-			} else {
-				
+				this.karClient.tell(karParams.service, karParams.path, karParams.params);
 				return Response.status(Response.Status.OK).build();
-				
+			} else {
+				return Response.status(Response.Status.OK).build();
 			}
 		} catch (Exception ex) {
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex).build();
 		}
-
 	}
 
 }
-
