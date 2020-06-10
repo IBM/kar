86827cc04c4442a4096bdf554a465cca8913e5e2
Paul C Castro <castrop@us.ibm.com>
2020-06-10 16:02:59 -0400

    Fixes #176 - enable http/2 for openliberty in actor-server (#177)

---
+++ scripts/setup-kind-kar-macos.sh
@@ -0,0 +1,29 @@
+############################################################
+# Install KAR into kind cluster on macos
+#
+# Prerequisites: Docker and Kind should be installed already
+# versions: Docker version 19.03.8, build afacb8b
+#           kind v0.8.1 go1.14.2 darwin/amd64
+#############################################################
+
+#!/bin/sh
+set -e
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/.."
+
+echo "Running KAR setup ...."
+cd $ROOTDIR
+$SCRIPTDIR/start-kind.sh
+make kindPushDev
+$SCRIPTDIR/kar-deploy.sh -dev
+
+echo "Building kar CLI"
+go install ./...
+
+echo "Setting up namespace and environment"
+$SCRIPTDIR/kar-enable-namespace.sh default
+
+source $SCRIPTDIR/kar-kind-env.sh
+
+echo "KAR Setup Complete!"
