dc2e9c1e3aedec8935805d80645b9790fc84a07c
David Grove <groved@us.ibm.com>
2020-10-08 17:47:07 -0400

    ykt server does not provide a service; update script

--- examples/actors-ykt/deploy/runServerLocally.sh
+++ examples/actors-ykt/deploy/runServerLocally.sh
@@ -6,4 +6,4 @@ CODEDIR="$SCRIPTDIR/.."
 KAR_VERBOSE=${KAR_VERBOSE:="info"}
 KAR_ACTOR_REMINDER_INTERVAL=${KAR_ACTOR_REMINDER_INTERVAL:="100ms"}
 
-kar run -v $KAR_VERBOSE -actor_reminder_interval $KAR_ACTOR_REMINDER_INTERVAL -app ykt -service simulation -actors Company,Site,Office,Researcher node $CODEDIR/ykt.js
+kar run -v $KAR_VERBOSE -actor_reminder_interval $KAR_ACTOR_REMINDER_INTERVAL -app ykt -actors Company,Site,Office,Researcher node $CODEDIR/ykt.js
