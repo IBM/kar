cbfade97e5b3e3151f17294c120e782ef42a7d3e
Olivier Tardieu <tardieu@us.ibm.com>
2020-10-26 10:58:07 -0400

    Obtain kafka brokers from kube secret

--- examples/camel-k/run-k8s-http-source.sh
+++ examples/camel-k/run-k8s-http-source.sh
@@ -1,6 +1,11 @@
 SCRIPTDIR=../../scripts
 
-kamel run --name=source \
-          $SCRIPTDIR/kamel/CloudEventProcessor.java \
-          -p camel.component.kafka.brokers=${KAR_KAFKA_CLUSTER_IP}:9092 \
-          input.yaml --dev
+set -e
+
+SECRET=$(kubectl get -n kar-system secret/kar.ibm.com.runtime-config -o json)
+BROKERS=$(echo $SECRET | jq -r .data.kafka_brokers | base64 -D)
+
+kamel run \
+  -p camel.component.kafka.brokers=$BROKERS \
+  $SCRIPTDIR/kamel/CloudEventProcessor.java \
+  --name=source input.yaml --dev
--- examples/camel-k/run-k8s-slack-sink.sh
+++ examples/camel-k/run-k8s-slack-sink.sh
@@ -1,7 +1,11 @@
 SCRIPTDIR=../../scripts
 
-kamel run --name=sink \
-          $SCRIPTDIR/kamel/CloudEventProcessor.java \
-          -p camel.component.kafka.brokers=${KAR_KAFKA_CLUSTER_IP}:9092 \
-          -p camel.component.slack.webhookUrl=${SLACK_KAR_OUTPUT_WEBHOOK} \
-          output.yaml --dev
+set -e
+
+SECRET=$(kubectl get -n kar-system secret/kar.ibm.com.runtime-config -o json)
+BROKERS=$(echo $SECRET | jq -r .data.kafka_brokers | base64 -D)
+
+kamel run \
+  -p camel.component.kafka.brokers=$BROKERS \
+  $SCRIPTDIR/kamel/CloudEventProcessor.java \
+  --name=sink output.yaml -p camel.component.slack.webhookUrl=${SLACK_KAR_OUTPUT_WEBHOOK} --dev
