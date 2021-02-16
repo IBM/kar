3e54b0474c3749d2925971bd9847d3fb80382219
Olivier Tardieu <tardieu@users.noreply.github.com>
2021-02-16 18:00:20 -0500

    Add misc example to demo KAR's fault-tolerance guarantees (#61)

---
+++ examples/misc/fault-tolerance/README.md
@@ -0,0 +1,34 @@
+<!--
+# Copyright IBM Corporation 2020,2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+-->
+
+# Fault-Tolerance in Action
+
+An example program to see KAR's fault-tolerance in action.
+
+In one window run:
+```shell
+kar run -app echo -service echo node server.js
+```
+
+In a second window run:
+```shell
+kar run -app echo -service_timeout 3600s node client.js
+```
+
+In the first window, kill the server while the second request is being
+processed. Then restart the server with the same command line as before. The new
+server instance should restart the interrupted request and respond to the
+client. The client should be unaffected.
---
+++ examples/misc/fault-tolerance/client.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright IBM Corporation 2020,2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ const fetch = require('node-fetch')
+
+function url (service, route) {
+  return `http://127.0.0.1:${process.env.KAR_RUNTIME_PORT}/kar/v1/service/${service}/call/${route}`
+}
+
+async function post (body) {
+  console.log('sending request with body', body)
+  const res = await fetch(url('echo', 'echo'), {
+    method: 'POST',
+    body,
+    headers: { 'Content-Type': 'text/plain' }
+  })
+  const text = await res.text()
+  console.log('received response with body', text)
+}
+
+async function main () {
+  await post('Joe')
+  await post('Jack')
+  await post('Josh')
+}
+
+main()
