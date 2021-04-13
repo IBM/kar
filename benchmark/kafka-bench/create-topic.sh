12a42e9ba06d7192d0682678c961c1363683bae5
David Grove <dgrove-oss@users.noreply.github.com>
2021-04-12 21:26:16 -0400

    create both topics expected by the benchmark (#94)

--- benchmark/kafka-bench/create-topic.sh
+++ benchmark/kafka-bench/create-topic.sh
@@ -1,3 +1,4 @@
 # Topic names: simple-topic, return-topic
 kubectl exec kar-kafka-0 -n kar-system -- /opt/kafka/bin/kafka-topics.sh --bootstrap-server :9092 --create --topic return-topic
+kubectl exec kar-kafka-0 -n kar-system -- /opt/kafka/bin/kafka-topics.sh --bootstrap-server :9092 --create --topic simple-topic
 
