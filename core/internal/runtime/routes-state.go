IBM/kar

commit 3f1187d7084a492b48c7ef19303ddb539172adf4

use HMGET to optimize subMap get (#112)

core/internal/runtime/routes-state.go
@@ -306,21 +306,22 @@ func routeImplSubmapOps(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 			http.Error(w, fmt.Sprintf("HKEYS failed: %v", err), http.StatusInternalServerError)
 			return
 		}
+		mapVals, err := store.HMGet(stateKey, mapKeys)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("HMGET failed: %v", err), http.StatusInternalServerError)
+			return
+		}
 
-		// Construct the response map by looking up each subkey
+		// Construct the response map by splicing together mapKeys and mapVals
 		subkeyPrefix := nestedEntryKeyPrefix(mapName)
 		m := map[string]interface{}{}
 		for i := range mapKeys {
-			if vstr, err := store.HGet(stateKey, mapKeys[i]); err == store.ErrNil {
-				// Map contains nil for this key; elide the entry
-			} else if err != nil {
-				http.Error(w, fmt.Sprintf("HKEY failed: %v", err), http.StatusInternalServerError)
-				return
-			} else {
+			val := mapVals[i]
+			if val != "" {
 				userSubkey := strings.TrimPrefix(mapKeys[i], subkeyPrefix)
 				var userValue interface{}
-				if json.Unmarshal([]byte(vstr), &userValue) != nil {
-					http.Error(w, fmt.Sprintf("Failed to deserialize result of HGET: %v", err), http.StatusInternalServerError)
+				if json.Unmarshal([]byte(val), &userValue) != nil {
+					http.Error(w, fmt.Sprintf("Failed to deserialize value: %v", err), http.StatusInternalServerError)
 					return
 				}
 				m[userSubkey] = userValue

core/internal/store/store.go
@@ -25,9 +25,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gomodule/redigo/redis"
 	"github.com/IBM/kar.git/core/internal/config"
 	"github.com/IBM/kar.git/core/pkg/logger"
+	"github.com/gomodule/redigo/redis"
 )
 
 var (
@@ -210,6 +210,16 @@ func HDelMultiple(hash string, keys []string) (int, error) {
 		args[i+1] = keys[i]
 	}
 	return redis.Int(do("HDEL", args...))
+}
+
+//HMGet hash key[]
+func HMGet(hash string, keys []string) ([]string, error) {
+	args := make([]interface{}, len(keys)+1)
+	args[0] = hash
+	for i := range keys {
+		args[i+1] = keys[i]
+	}
+	return redis.Strings(do("HMGET", args...))
 }
 
 // HGetAll hash

commit 4d014d9faaedbcb756c9d3410645ed2ddba18671

subMap.getAll improvements
1. fix bug on getAll of empty submap introduced in 3f1187d7.
2. replace potentially long-latency HKEYS operation with HSCAN loop.

core/internal/runtime/routes-state.go
@@ -303,13 +303,16 @@ func routeImplSubmapOps(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 	case "get":
 		mapKeys, err := getSubMapKeys(stateKey, mapName)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("HKEYS failed: %v", err), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("getSubMapKeys failed: %v", err), http.StatusInternalServerError)
 			return
 		}
-		mapVals, err := store.HMGet(stateKey, mapKeys)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("HMGET failed: %v", err), http.StatusInternalServerError)
-			return
+		mapVals := make([]string, 0)
+		if len(mapKeys) > 0 {
+			mapVals, err = store.HMGet(stateKey, mapKeys)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("HMGET failed: %v", err), http.StatusInternalServerError)
+				return
+			}
 		}
 
 		// Construct the response map by splicing together mapKeys and mapVals
@@ -345,7 +348,7 @@ func routeImplSubmapOps(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 	case "size":
 		mapKeys, err := getSubMapKeys(stateKey, mapName)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("HKEYS failed: %v", err), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("getSubMapKeys failed: %v", err), http.StatusInternalServerError)
 			return
 		}
 		response = len(mapKeys)
@@ -374,21 +377,22 @@ func routeImplSubmapOps(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 }
 
 func getSubMapKeys(stateKey string, mapName string) ([]string, error) {
-	// TODO: Future performance optimization.
-	//       We should instead use HScan to incrementally accumulate the
-	//       list of keys to avoid long latency operations on Redis.
-	keys, err := store.HKeys(stateKey)
-	if err != nil {
-		return nil, err
-	}
-	subkeyPrefix := nestedEntryKeyPrefix(mapName)
-	flatKey := flatEntryKey(mapName)
 	mapKeys := []string{}
-	for i := range keys {
-		if keys[i] != flatKey && strings.HasPrefix(keys[i], subkeyPrefix) {
-			mapKeys = append(mapKeys, keys[i])
+	cursor := 0
+	subkeyPrefix := nestedEntryKeyPrefix(mapName) + "*"
+	for {
+		cursor, result, err := store.HScan(stateKey, cursor, subkeyPrefix)
+		if err != nil {
+			return nil, err
+		}
+		for curIndex := 0; curIndex < len(result); curIndex += 2 {
+			mapKeys = append(mapKeys, result[curIndex])
+		}
+		if cursor == 0 {
+			break
 		}
 	}
+
 	return mapKeys, nil
 }
 

core/internal/store/store.go
@@ -212,7 +212,7 @@ func HDelMultiple(hash string, keys []string) (int, error) {
 	return redis.Int(do("HDEL", args...))
 }
 
-//HMGet hash key[]
+// HMGet hash key[]
 func HMGet(hash string, keys []string) ([]string, error) {
 	args := make([]interface{}, len(keys)+1)
 	args[0] = hash
@@ -220,6 +220,27 @@ func HMGet(hash string, keys []string) ([]string, error) {
 		args[i+1] = keys[i]
 	}
 	return redis.Strings(do("HMGET", args...))
+}
+
+// HScan hash cursor [MATCH match]
+func HScan(hash string, cursor int, match string) (int, []string, error) {
+	var response []interface{}
+	var err error
+	if match != "" {
+		response, err = redis.Values(do("HSCAN", hash, cursor, "MATCH", match))
+	} else {
+		response, err = redis.Values(do("HSCAN", hash, cursor))
+	}
+	if err != nil {
+		return 0, nil, err
+	}
+	cursor, _ = strconv.Atoi(string(response[0].([]byte)))
+	data := response[1].([]interface{})
+	ans := make([]string, len(data))
+	for i := range data {
+		ans[i] = string(data[i].([]byte))
+	}
+	return cursor, ans, nil
 }
 
 // HGetAll hash