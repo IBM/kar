60d68a978461a4517d02005db784754bfa3e00f4
David Grove <dgrove-oss@users.noreply.github.com>
2021-07-30 15:33:52 -0400

    Move all non-trivial runtime functionality to kar-runtime-core (#135)
    Restructure code to move the implementation of the actor runtime
    to kar-runtime-core.  Simplify and cleanup code in several ways
    during the process of moving it.
    
    Now all that is left in kar-runtime-liberty is the Open Liberty
    specific code that defines the REST APIs and handles server
    initialization and configuration.

--- sdk-java/kar-runtime-liberty/src/main/java/com/ibm/research/kar/liberty/ActorRuntimeResource.java
+++ sdk-java/kar-runtime-liberty/src/main/java/com/ibm/research/kar/liberty/ActorRuntimeResource.java
@@ -16,17 +16,7 @@
 
 package com.ibm.research.kar.liberty;
 
-import java.lang.invoke.MethodHandle;
-
-import java.io.StringWriter;
-import java.io.PrintWriter;
-
-import javax.inject.Inject;
-import javax.json.Json;
 import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonValue;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -40,9 +30,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import com.ibm.research.kar.Kar;
-import com.ibm.research.kar.actor.ActorInstance;
 import com.ibm.research.kar.runtime.ActorManager;
-import com.ibm.research.kar.runtime.KarConfig;
 
 @Path("actor")
 public class ActorRuntimeResource {
@@ -51,50 +39,13 @@ public class ActorRuntimeResource {
 	@Path("{type}/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response getActor(@PathParam("type") String type, @PathParam("id") String id) {
-		if (ActorManager.getActor(type, id) != null) {
-			// Already exists; nothing to do.
-			return Response.status(Response.Status.OK).build();
-		}
-
-		// Allocate a new actor instance
-		ActorInstance actorObj = ActorManager.createActor(type, id);
-		if (actorObj == null) {
-			return Response.status(Response.Status.NOT_FOUND).entity("Not found: " + type + " actor " + id).build();
-		}
-
-		// Call the optional activate method
-		try {
-			MethodHandle activate = ActorManager.getActorActivateMethod(type);
-			if (activate != null) {
-				activate.invoke(actorObj);
-			}
-			return Response.status(Response.Status.CREATED).entity("Created " + type + " actor " + id).build();
-		} catch (Throwable t) {
-			return Response.status(Response.Status.BAD_REQUEST).entity(t.toString()).build();
-		}
+		return ActorManager.activateInstanceIfNotPresent(type, id);
 	}
 
 	@DELETE
 	@Path("{type}/{id}")
 	public Response deleteActor(@PathParam("type") String type, @PathParam("id") String id) {
-		ActorInstance actorObj = ActorManager.getActor(type, id);
-		if (actorObj == null) {
-			return Response.status(Response.Status.NOT_FOUND).entity("Not found: " + type + " actor " + id).build();
-		}
-
-		// Call the optional deactivate method
-		MethodHandle deactivate = ActorManager.getActorDeactivateMethod(type);
-		if (deactivate != null) {
-			try {
-				deactivate.invoke(actorObj);
-			} catch (Throwable t) {
-				return Response.status(Response.Status.BAD_REQUEST).entity(t.toString()).build();
-			}
-		}
-
-		// Actually remove the instance
-		ActorManager.deleteActor(type, id);
-		return Response.status(Response.Status.OK).build();
+		return ActorManager.deactivateInstanceIfPresent(type, id);
 	}
 
 	@HEAD
@@ -110,61 +61,6 @@ public class ActorRuntimeResource {
 	@Produces(Kar.KAR_ACTOR_JSON)
 	public Response invokeActorMethod(@PathParam("type") String type, @PathParam("id") String id,
 			@PathParam("sessionid") String sessionid, @PathParam("path") String path, JsonArray args) {
-
-		ActorInstance actorObj = ActorManager.getActor(type, id);
-		if (actorObj == null) {
-			return Response.status(Response.Status.NOT_FOUND).type(MediaType.TEXT_PLAIN).entity("Actor instance not found: " + type + "[" + id +"]").build();
-		}
-
-		MethodHandle actorMethod = ActorManager.getActorMethod(type, path, args.size());
-		if (actorMethod == null) {
-			return Response.status(Response.Status.NOT_FOUND).type(MediaType.TEXT_PLAIN).entity("Method not found: " + type + "." + path + " with " + args.size() + " arguments").build();
-		}
-
-		// set the session
-		actorObj.setSession(sessionid);
-
-		// build arguments array for method handle invoke
-		Object[] actuals = new Object[args.size() + 1];
-		actuals[0] = actorObj;
-		for (int i = 0; i < args.size(); i++) {
-			actuals[i + 1] = args.get(i);
-		}
-
-		try {
-			Object result = actorMethod.invokeWithArguments(actuals);
-			if (result == null && actorMethod.type().returnType().equals(Void.TYPE)) {
-				return Response.status(Response.Status.NO_CONTENT).build();
-			} else {
-				JsonValue jv = result != null ? (JsonValue)result : JsonValue.NULL;
-				JsonObject ro = Json.createObjectBuilder().add("value", jv).build();
-				return Response.status(Response.Status.OK).type(Kar.KAR_ACTOR_JSON).entity(ro).build();
-			}
-		} catch (Throwable t) {
-			if (KarConfig.SHORTEN_ACTOR_STACKTRACES) {
-				// Elide all of the implementation details above us in the backtrace
-				StackTraceElement [] fullBackTrace = t.getStackTrace();
-				for (int i=0; i<fullBackTrace.length; i++) {
-					if (fullBackTrace[i].getClassName().equals(ActorRuntimeResource.class.getName()) && fullBackTrace[i].getMethodName().equals("invokeActorMethod")) {
-						StackTraceElement[] reducedBackTrace = new StackTraceElement[i+1];
-						System.arraycopy(fullBackTrace, 0, reducedBackTrace, 0, i+1);
-						t.setStackTrace(reducedBackTrace);
-						break;
-					}
-				}
-			}
-			JsonObjectBuilder ro = Json.createObjectBuilder();
-			ro.add("error", true);
-			ro.add("message", t.toString());
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			t.printStackTrace(pw);
-			String backtrace = sw.toString();
-			if (backtrace.length() > KarConfig.MAX_STACKTRACE_SIZE) {
-				backtrace = backtrace.substring(0, KarConfig.MAX_STACKTRACE_SIZE) + "\n...Backtrace truncated due to message length restrictions\n";
-			}
-			ro.add("stack", sw.toString());
-			return Response.status(Response.Status.OK).type(Kar.KAR_ACTOR_JSON).entity(ro.build()).build();
-		}
+		return ActorManager.invokeActorMethod(type, id, sessionid, path, args);
 	}
 }
