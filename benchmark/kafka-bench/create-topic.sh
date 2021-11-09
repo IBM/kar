e1e22ccdca873f39812294f01ea9ab113c17933e
David Grove <groved@us.ibm.com>
2021-11-09 13:50:17 -0500

    bug fix: require # of replicas when creating topics for benchmark

--- benchmark/kafka-bench/create-topic.sh
+++ benchmark/kafka-bench/create-topic.sh
@@ -1,4 +1,9 @@
 # Topic names: simple-topic, return-topic
-kubectl exec kar-kafka-0 -n kar-system -- /opt/kafka/bin/kafka-topics.sh --bootstrap-server :9092 --create --topic return-topic
-kubectl exec kar-kafka-0 -n kar-system -- /opt/kafka/bin/kafka-topics.sh --bootstrap-server :9092 --create --topic simple-topic
+
+rf=$1
+
+echo "creating topic with replication-factor $rf"
+
+kubectl exec kar-kafka-0 -n kar-system -- /opt/kafka/bin/kafka-topics.sh --bootstrap-server :9092 --create --replication-factor $rf --topic return-topic
+kubectl exec kar-kafka-0 -n kar-system -- /opt/kafka/bin/kafka-topics.sh --bootstrap-server :9092 --create --replication-factor $rf --topic simple-topic
 
