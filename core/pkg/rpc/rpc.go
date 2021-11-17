d89331fdbed7661aa9026a739d0dcbc01b669a57
David Grove <groved@us.ibm.com>
2021-11-17 14:43:50 -0500

    generalize cmdline support for configuring kafka topic used by KAR

--- core/pkg/rpc/rpc.go
+++ core/pkg/rpc/rpc.go
@@ -29,7 +29,7 @@ type Config struct {
 	Password      string   // Kafka SASL password
 	EnableTLS     bool
 	TLSSkipVerify bool
-	Retention     time.Duration // Message retention time
+	TopicConfig   map[string]*string
 }
 
 // Target of an invocation
