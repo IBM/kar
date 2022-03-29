28fc5c7af7c910324e1895807801cc689dd343d9
David Grove <groved@us.ibm.com>
2022-03-29 11:19:36 -0400

    extract rebalance timestamps from the kafka log

--- core/cmd/kar-log-analyzer/main.go
+++ core/cmd/kar-log-analyzer/main.go
@@ -14,6 +14,19 @@
 // limitations under the License.
 //
 
+// This program is used to correlate events in the log files from
+// the k3d fault-driver, kar leader, and kafka logs collected during
+// a failure scenario run to enable analysis of performance during failures.
+//
+// From the fault-driver log, it extracts the the start time of
+// each failure and the maximum order latency during the failure.
+// From the kafka log, it extracts the start and end time of Kafka rebalances.
+// From the kar log, it extracts the end time of the KAR recovery.
+//
+// Run the experiment using the scripts found in kar-apps/reefer/scripts.
+// Collect the kakfa log, kar sidecar log from the leader (will be
+// either the monitor or simulator sidecar), and the fault driver log.
+
 package main
 
 import (
@@ -29,16 +42,17 @@ import (
 var (
 	karlog       string
 	applog       string
-	rebalances   []rebalanceEvent = []rebalanceEvent{}
+	kafkalog     string
 	failures     []failureEvent   = []failureEvent{}
+	rebalances   []rebalanceEvent = []rebalanceEvent{}
+	processing   []time.Time      = []time.Time{}
 	summaries    []summary        = []summary{}
 	failureHisto []int            = []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	kafkaDetect                   = 10 * time.Second // assume default, will update with value parsed from kar leader log
 )
 
 type rebalanceEvent struct {
 	startTime time.Time
-	duration  time.Duration
+	endTime   time.Time
 }
 
 type failureEvent struct {
@@ -47,18 +61,19 @@ type failureEvent struct {
 }
 
 type summary struct {
-	startTime          time.Time
-	totalDuration      time.Duration
-	maximumOrder       time.Duration
-	kafkaDetection     time.Duration
-	karDetection       time.Duration
-	karReconcilliation time.Duration
-	failureCount       int
+	startTime       time.Time
+	totalDuration   time.Duration
+	maximumOrder    time.Duration
+	detection       time.Duration
+	consensus       time.Duration
+	reconcilliation time.Duration
+	failureCount    int
 }
 
 func init() {
-	flag.StringVar(&karlog, "k", "", "file name of sidecar log to process")
-	flag.StringVar(&applog, "a", "", "file name of fault driver log to process")
+	flag.StringVar(&kafkalog, "kafka", "", "file name of kafka log to process")
+	flag.StringVar(&karlog, "kar", "", "file name of sidecar log to process")
+	flag.StringVar(&applog, "app", "", "file name of fault driver log to process")
 
 	flag.CommandLine.Parse(os.Args[1:])
 }
@@ -70,9 +85,6 @@ func readKarLog() {
 	}
 	defer file.Close()
 
-	startTime := time.Time{}
-	recovering := false
-
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -82,28 +94,12 @@ func readKarLog() {
 			if err != nil {
 				panic(fmt.Errorf("Can't parse time %v: %v", msg[0], err))
 			}
-			if strings.Contains(msg[1], "kafka: session.timeout.ms") {
-				tmp := strings.Split(msg[1], "=")
-				ms, err := strconv.Atoi(strings.TrimSpace(tmp[1]))
-				if err == nil {
-					kafkaDetect = time.Duration(ms) * time.Millisecond
-				}
-			}
-
-			if !recovering && strings.Contains(msg[1], "completed generation") {
-				startTime = ts
-				recovering = true
-			} else if strings.Contains(msg[1], "processing messages") {
-				if recovering {
-					outage := ts.Sub(startTime)
-					rebalances = append(rebalances, rebalanceEvent{startTime: startTime, duration: outage})
-					startTime = time.Time{}
-					recovering = false
-				}
+			if strings.Contains(msg[1], "processing messages") {
+				processing = append(processing, ts)
 			}
 		}
 	}
-	fmt.Printf("Parsed %v rebalance events\n", len(rebalances))
+	fmt.Printf("Found %v processing events\n", len(rebalances))
 }
 
 func readAppLog() {
@@ -120,7 +116,7 @@ func readAppLog() {
 		if strings.Contains(line, "k3d node stop") {
 			tmp := strings.Split(line, "k3d")[0]
 			tmp = strings.Replace(tmp, ", 24:", ", 00:", 1)
-			ts, err := time.Parse("01/02/2006, 15:04:05", strings.TrimSpace(tmp))
+			ts, err := time.Parse("1/2/2006, 15:04:05", strings.TrimSpace(tmp))
 			if err != nil {
 				panic(fmt.Errorf("Can't parse time %v: %v", tmp, err))
 			}
@@ -141,46 +137,83 @@ func readAppLog() {
 	fmt.Printf("Parsed %v failure events\n", len(failures))
 }
 
+func readKafkaLog() {
+	file, err := os.Open(kafkalog)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	prepare := time.Time{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		msg := strings.Split(line, " INFO ")
+		if len(msg) == 2 {
+			ts, err := time.Parse("[2006-01-02 15:04:05]", strings.TrimSpace(msg[0]))
+			if err != nil {
+				panic(fmt.Errorf("Can't parse time %v: %v", msg[0], err))
+			}
+			if strings.Contains(msg[1], "Preparing to rebalance group") {
+				prepare = ts
+			} else if strings.Contains(msg[1], "Stabilized group") {
+				rebalances = append(rebalances, rebalanceEvent{startTime: prepare, endTime: ts})
+				prepare = time.Time{}
+			}
+		}
+	}
+	fmt.Printf("Parsed %v kafka rebalances\n", len(rebalances))
+}
+
 func correlateLogs() {
-	rebalanceIdx := 0
 	failureIdx := 0
+	rebalanceIdx := 0
+	processingIdx := 0
 	for ; failureIdx < len(failures); failureIdx += 1 {
 		pf := failures[failureIdx]
 		for rebalances[rebalanceIdx].startTime.Before(pf.startTime) {
 			rebalanceIdx += 1
 		}
 		r := rebalances[rebalanceIdx]
+		for processing[processingIdx].Before(r.endTime) {
+			processingIdx += 1
+		}
+		p := processing[processingIdx]
 		maxOrder := pf.maximumOrder
 		numFailures := 1
-		for ; failureIdx+1 < len(failures) && (r.startTime.Add(r.duration)).After(failures[failureIdx+1].startTime); failureIdx += 1 {
+		for ; failureIdx+1 < len(failures) && p.After(failures[failureIdx+1].startTime); failureIdx += 1 {
 			sf := failures[failureIdx+1]
-			// fmt.Printf("Merging failure: %v and %v due to rebalance %v\n", pf, sf, r)
+			// fmt.Printf("Merging failure: %v and %v due to processing %v\n", pf, sf, p)
 			numFailures += 1
 			if sf.maximumOrder > maxOrder {
 				maxOrder = sf.maximumOrder
 			}
 		}
 
-		outage := r.startTime.Sub(pf.startTime) + r.duration
+		outage := p.Sub(pf.startTime)
 		detect := r.startTime.Sub(pf.startTime)
-		summaries = append(summaries, summary{startTime: pf.startTime, failureCount: numFailures, totalDuration: outage, kafkaDetection: kafkaDetect,
-			karDetection: detect - kafkaDetect, karReconcilliation: r.duration, maximumOrder: maxOrder})
+		rebalance := r.endTime.Sub(r.startTime)
+		reconcile := p.Sub(r.endTime)
+		summaries = append(summaries, summary{startTime: pf.startTime, failureCount: numFailures, totalDuration: outage, detection: detect,
+			consensus: rebalance, reconcilliation: reconcile, maximumOrder: maxOrder})
 		failureHisto[numFailures] += 1
 	}
 	fmt.Printf("Count of failure clusters: %v\n", failureHisto[1:])
 }
 
 func printSummary() {
-	fmt.Printf("Start Time, Failure Number, Num Failures, Kafka Detection, KAR Detection, KAR Reconcilliation, Total Outage, Max Order Latency\n")
+	fmt.Printf("Start Time, Failure Number, Num Failures, Detection, Consensus, Reconcilliation, Total Outage, Max Order Latency\n")
 	for i, f := range summaries {
-		fmt.Printf("%v, %v, %v, %.6v, %.6v, %.6v, %.6v, %.6v\n", f.startTime, i+1, f.failureCount, f.kafkaDetection.Seconds(), f.karDetection.Seconds(),
-			f.karReconcilliation.Seconds(), f.totalDuration.Seconds(), f.maximumOrder.Seconds())
+		fmt.Printf("%v, %v, %v, %.6v, %.6v, %.6v, %.6v, %.6v\n", f.startTime, i+1, f.failureCount, f.detection.Seconds(), f.consensus.Seconds(),
+			f.reconcilliation.Seconds(), f.totalDuration.Seconds(), f.maximumOrder.Seconds())
 	}
 }
 
 func main() {
+	fmt.Printf("input kafka log: %v\n", kafkalog)
 	fmt.Printf("input kar log: %v\n", karlog)
 	fmt.Printf("input app log: %v\n", applog)
+	readKafkaLog()
 	readKarLog()
 	readAppLog()
 	correlateLogs()
