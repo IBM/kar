53086c537708fdf4e2c94cf936b5c2f85bc66568
David Grove <dgrove-oss@users.noreply.github.com>
2023-05-03 15:27:05 -0400

    update endyear in copyright statement to 2023 (#371)
    Signed-off-by: David Grove <groved@us.ibm.com>

--- sdk-js/index.d.ts
+++ sdk-js/index.d.ts
@@ -2,7 +2,7 @@
 // Project:[KAR:Kubernetes Application Runtime]
 
 /*
- * Copyright IBM Corporation 2020,2022
+ * Copyright IBM Corporation 2020,2023
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
